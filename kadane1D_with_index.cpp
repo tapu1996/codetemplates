1ddd4340f593a36b581955787db423c102ccb9b0
tapu1996 <37135894+tapu1996@users.noreply.github.com>
2018-04-13 15:14:23 +0600

    Update kadane1D_with_index.cpp

--- kadane1D_with_index.cpp
+++ kadane1D_with_index.cpp
@@ -5,7 +5,50 @@
 #include<iostream>
 #include<climits>
 using namespace std;
- 
+
+///for finding number of times of occurence of max sum sequence
+
+void maxSubArraySum(vector<int>a)
+{
+   int max_so_far = a[0];
+  int size= a.size();
+   int s[size];
+   int counts[size];
+   s[0]=a[0];
+   counts[0]=1;
+
+   for (int i = 1; i < size; i++)
+   {
+
+        int temp = s[i-1]+a[i];
+        if(temp==a[i])
+        {
+           s[i]=temp;
+           counts[i]=counts[i-1]+1;
+        }
+        else
+        {
+           if(a[i]>temp)
+           {
+              s[i]=a[i];
+              counts[i]=1;
+           }
+           else
+           {
+              s[i]=temp;
+              counts[i]=counts[i-1];
+           }
+        }
+        max_so_far = max(max_so_far, s[i]);
+   }
+
+   long long ans=0;
+   for(int i=0;i<size;++i)
+         if(s[i]==max_so_far)
+            ans+=counts[i];
+      printf("%d %lld\n",max_so_far,ans);
+}
+
 int maxSubArraySum(int a[], int size)
 {
     int max_so_far = INT_MIN, max_ending_here = 0,
